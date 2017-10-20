b334d57dfdf72ce4df628d8cdeb24e5d88496f93
Guotai Wang <wguotai@gmail.com>
2017-10-20 15:08:53 +0100

    update random flip

--- image_io/data_generator.py
+++ image_io/data_generator.py
@@ -19,9 +19,13 @@ def random_flip_tensors_in_one_dim(x, d):
     d: a integer denoting the axis
     """
     r = tf.random_uniform([], 0, 1)
-    if(tf.less(r, tf.constant(0.5))):
-       x = [tf.reverse(xi, tf.constant(d)) for xi in x]
-    return x
+    r = tf.less(r, tf.constant(0.5))
+    r = tf.cast(r, tf.int32)
+    y = []
+    for xi in x:
+        xi_xiflip = [xi, tf.reverse(xi, tf.constant(d))]
+        y.append(xi_xiflip[r])
+    return y
 
 class ImageDataGenerator(object):
     """Wrapper class around the new Tensorflows dataset pipeline.
