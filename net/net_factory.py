ccd99d81b4b80ce9732499370e032eea4cb06e58
Guotai Wang <wguotai@gmail.com>
2017-11-07 13:47:29 +0000

    add pnet_ms

--- net/net_factory.py
+++ net/net_factory.py
@@ -5,6 +5,7 @@ import os
 import sys
 from net.unet2d import UNet2D
 from net.pnet import PNet
+from net.pnet_ms import PNet_Multi_Slice
 from net.vgg21 import VGG21
 class NetFactory(object):
     @staticmethod
@@ -13,6 +14,8 @@ class NetFactory(object):
             return UNet2D
         if name == 'PNet':
             return PNet
+        if name == 'PNet_Multi_Slice':
+            return PNet_Multi_Slice
         if name == 'VGG21':
             return VGG21
         print('unsupported network:', name)
