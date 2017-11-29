299d2103576a0c20a314112e1df29ff8c7af71b9
Guotai Wang <wguotai@gmail.com>
2017-11-29 18:12:47 +0000

    update networks

--- net/net_factory.py
+++ net/net_factory.py
@@ -5,11 +5,7 @@ import os
 import sys
 from Demic.net.unet2d import UNet2D
 from Demic.net.pnet import PNet
-from Demic.net.pnet_multi_slice import PNet_Multi_Slice
-from Demic.net.pnet_multi_slice_deep_fuse import PNet_Multi_Slice_Deep_Fuse
-from Demic.net.pnet_stn import PNet_STN
-from Demic.net.pnet_stn_deep_fuse import PNet_STN_DF
-from Demic.net.pnet_stn_deep_weight_fuse import PNet_STN_WDF
+from Demic.net.pnet_stn_fuse import PNet_STN_DF
 from Demic.net.vgg21 import VGG21
 class NetFactory(object):
     @staticmethod
@@ -18,16 +14,8 @@ class NetFactory(object):
             return UNet2D
         if name == 'PNet':
             return PNet
-        if name == 'PNet_Multi_Slice':
-            return PNet_Multi_Slice
-        if name == 'PNet_Multi_Slice_Deep_Fuse':
-            return PNet_Multi_Slice_Deep_Fuse
-        if name == 'PNet_STN':
-            return PNet_STN
         if name == 'PNet_STN_DF':
             return PNet_STN_DF
-        if name == 'PNet_STN_WDF':
-            return PNet_STN_WDF
         if name == 'VGG21':
             return VGG21
         print('unsupported network:', name)
