cd89f971f8a990caa915dbed65c2a51cae8e4592
Guotai Wang <wguotai@gmail.com>
2017-11-29 18:06:27 +0000

    update pnet_stn_fuse

--- net/pnet_stn_fuse.py
+++ net/pnet_stn_fuse.py
@@ -29,27 +29,27 @@ def w_initializer_near_zero():
             shape, 0.0, stddev, dtype=tf.float32)
     return _initializer
 
-class Weight_Net(TrainableLayer):
+class Fuse_Net(TrainableLayer):
     """
-    Weight_Net, return normalized weight for each slice
-    The weight of the central slice is initalized as 1.0, other slices 0.0
+    Fuse_Net, 
     input size: [N, D, H, W, C]
-    output size:[N, D, H, W, C]
+    output size:[N, D, H, W, Cl], Cl is the number of classes
     """
     def __init__(self,
-                 num_slices,
+                 num_classes,
                  parameters   =None,
                  w_initializer=None,
                  w_regularizer=None,
                  b_initializer=None,
                  b_regularizer=None,
                  acti_func='prelu',
-                 name='Weight_Net'):
-        super(Weight_Net, self).__init__(name=name)
-        self.parameters = parameters
-        self.acti_func = acti_func
-        self.num_slices = num_slices
-        self.n_features = [16, 16, 16, 16]
+                 name='Fuse_Net'):
+        super(Fuse_Net, self).__init__(name=name)
+        self.parameters   = parameters
+        self.acti_func    = acti_func
+        self.num_classes  = num_classes
+        self.n_features   = [16, 16, 16, 16]
+        self.dilations    = [1,  2,  3,  1]
         self.initializers = {'w': w_initializer, 'b': b_initializer}
         self.regularizers = {'w': w_regularizer, 'b': b_regularizer}
         
@@ -58,51 +58,43 @@ class Weight_Net(TrainableLayer):
     def layer_op(self, images, is_training, bn_momentum=0.9, layer_id=-1):
         conv1 = ConvolutionalLayer(n_output_chns = self.n_features[0],
                                  kernel_size = [1, 3, 3],
+                                 dilation = [1, self.dilations[0]],
                                  w_initializer=self.initializers['w'],
                                  w_regularizer=self.regularizers['w'],
                                  name='conv1')
         conv2 = ConvolutionalLayer(n_output_chns = self.n_features[1],
                                  kernel_size = [1, 3, 3],
+                                 dilation = [1, self.dilations[1]],
                                  w_initializer=self.initializers['w'],
                                  w_regularizer=self.regularizers['w'],
                                  name='conv2')
         conv3 = ConvolutionalLayer(n_output_chns = self.n_features[2],
                                  kernel_size = [1, 3, 3],
+                                 dilation = [1, self.dilations[2]],
                                  w_initializer=self.initializers['w'],
                                  w_regularizer=self.regularizers['w'],
                                  name='conv3')
         conv4 = ConvolutionalLayer(n_output_chns = self.n_features[3],
                                  kernel_size = [1, 3, 3],
+                                 dilation = [1, self.dilations[3]],
                                  w_initializer=self.initializers['w'],
                                  w_regularizer=self.regularizers['w'],
                                  name='conv4')
-        conv5_list = []
-        for i in range(self.num_slices):
-            w_initializer = self.initializers['w']
-            if(i != (self.num_slices-1)/2):
-                w_initializer = w_initializer_near_zero()
-            conv5_i =ConvolutionalLayer(n_output_chns = 1,
-                                    kernel_size = [1, 3, 3],
-                                    w_initializer=w_initializer,
-                                    w_regularizer=self.regularizers['w'],
-                                    name='conv5_{}'.format(i))
-            conv5_list.append(conv5_i)
-        output = tf.transpose(images, perm= [0, 4, 2, 3, 1])
-        output = conv1(output, is_training, bn_momentum)
+        conv5 = ConvolutionalLayer(n_output_chns = self.num_classes,
+                                 kernel_size = [1, 3, 3],
+                                 w_initializer=self.initializers['w'],
+                                 w_regularizer=self.regularizers['w'],
+                                 name='conv5')
+
+        output = conv1(images, is_training, bn_momentum)
         output = conv2(output, is_training, bn_momentum)
         output = conv3(output, is_training, bn_momentum)
         output = conv4(output, is_training, bn_momentum)
-        conv5_out_list = []
-        for i in range(self.num_slices):
-            temp_conv5_out = conv5_list[i](output, is_training, bn_momentum)
-            conv5_out_list.append(temp_conv5_out)
-        output = tf.concat(conv5_out_list, axis = -1)
-        output = tf.nn.softmax(output)
-        output = tf.transpose(output, perm = [0, 4, 2, 3, 1])
+        output = conv5(output, is_training, bn_momentum)
         return output
 
 
-class PNet_STN_WDF(TrainableLayer):
+class PNet_STN_DF(TrainableLayer):
     """
         PNet_STN_DF
         The input tensor shape is [N, D, H, W, C]
@@ -121,8 +113,8 @@ class PNet_STN_WDF(TrainableLayer):
                  b_initializer=None,
                  b_regularizer=None,
                  acti_func='prelu',
-                 name='PNet_STN_WDF'):
-        super(PNet_STN_WDF, self).__init__(name=name)
+                 name='PNet_STN_DF'):
+        super(PNet_STN_DF, self).__init__(name=name)
         self.parameters = parameters
         self.acti_func = acti_func
         self.num_classes = num_classes
@@ -143,19 +135,25 @@ class PNet_STN_WDF(TrainableLayer):
                           w_regularizer=self.regularizers['w'],
                           acti_func=self.acti_func,
                           name = 'pnet_layer')
-        weight_layer = Weight_Net(self.input_shape[1],
+        fuse_layer = Weight_Net(self.num_classes,
                                   w_initializer=self.initializers['w'],
                                   w_regularizer=self.regularizers['w'],
                                   acti_func=self.acti_func,
-                                  name = 'weight_layer')
+                                  name = 'fuse_layer')
 
         if (self.parameters['slice_fusion'] == True):
-            img_aligned = stn_layer(images, is_training, bn_momentum)
-            output = pnet_layer(img_aligned, is_training, bn_momentum)
-            weight = weight_layer(img_aligned, is_training, bn_momentum)
-            print(tf.shape(output), tf.shape(weight))
-            output = output*weight
-            output = tf.reduce_sum(output, axis = 1, keep_dims = True)
+            aligned = stn_layer(images, is_training, bn_momentum)
+            output0 = pnet_layer(aligned, is_training, bn_momentum)
+            
+            [N, D, H, W, C] = self.input_shape
+            aligned_reshape = tf.transpose(aligned, perm = [0, 2, 3, 1, 4])
+            aligned_reshape = tf.reshape(aligned_reshape, [D, 1, H, W, -1])
+            
+            output0_reshape = tf.transpose(output0, perm = [0, 2, 3, 1, 4])
+            output0_reshape = tf.reshape(output0_reshape, [D, 1, H, W, -1])
+            
+            fuse_input = tf.concat([aligned_reshape, output0_reshape], axis = -1)
+            output = fuse_layer(fuse_input, is_training, bn_momentum)
         else:
             print('slice fusion is false')
             output = pnet_layer(images, is_training, bn_momentum)
